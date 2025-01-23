f2ddb076ea31ea112b05d593626eea6aca302655
Carol Savio <161618640+Aploria@users.noreply.github.com>
2025-01-23 15:28:16 -0300

    Adicionando amigos a lista e verificando se o campo está vazio

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+//Adicionando amigos a lista e verificando se o campo está vazio
+function adicionarAmigo(){
+    let nomeAmigo = document.getElementById('amigo').value;
+    if (nomeAmigo == "") {
+        alert('Por favor, insira um nome abaixo!'); 
+    } else {
+        amigos.push(nomeAmigo);
+    }
+    console.log(amigos);
+    limpaCampo();
+}
+// função para limpar o campo de input
+function limpaCampo(){
+    let nomeAmigo = document.getElementById('amigo');
+    nomeAmigo.value = "";
+}
+
+    
